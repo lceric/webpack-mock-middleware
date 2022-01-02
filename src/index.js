c5953842bac1d83ce169255a1048873382c6a5a0
Eric Lee <lichao@kaike.la>
2022-01-02 16:23:59 +0800

    fix: use origin resData

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-const cwdPath = process.cwd();
-const fs = require("fs");
-const path = require("path");
+const cwdPath = process.cwd()
+const fs = require('fs')
+const path = require('path')
 module.exports = (req, res, next) => {
-  let mockPath = path.join(cwdPath, "mock" + req.path + ".js");
-  delete require.cache[mockPath];
-  fs.open(mockPath, "r", err => {
+  let mockPath = path.join(cwdPath, 'mock' + req.path + '.js')
+  delete require.cache[mockPath]
+  fs.open(mockPath, 'r', (err) => {
     if (err) {
-      if (err.code === "ENOENT") {
-        console.warn("[fail mock] " + req.path);
-        next();
-        return;
+      if (err.code === 'ENOENT') {
+        console.warn('[fail mock] ' + req.path)
+        next()
+        return
       }
-      throw err;
+      throw err
     }
     let generateMock = require(mockPath)
-    let isFun = typeof generateMock === "function"
-    let resData = isFun ?
-        generateMock(req, res, next)
-        : generateMock;
-    res.json({ ...resData });
-  });
-};
+    let isFun = typeof generateMock === 'function'
+    let resData = isFun ? generateMock(req, res, next) : generateMock
+    res.json(resData)
+  })
+}
